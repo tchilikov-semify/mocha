dfac9c0b082f0b5094cbb2d5975ab007c3f75eac
Rupert Swarbrick <rswarbrick@gmail.com>
2026-06-13 23:07:03 +0100

    [dv,axi] Implement items for AXI read transactions
    The axi_read_request_item and axi_read_data_item classes are intended
    to be randomised (when a sequence wishes to send either read requests
    or data responses). The axi_read_item class can be used by a monitor
    that sees a read request and then one or more responses.

---
+++ hw/ip/dv/axi_agent/axi_agent_pkg.sv
@@ -0,0 +1,18 @@
+// Copyright lowRISC contributors
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+package axi_agent_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  // The possible encodings of the AxBURST signal
+  typedef enum bit [1:0] {
+    BurstFixed = 0,
+    BurstIncr  = 1,
+    BurstWrap  = 2
+  } burst_e;
+
+  `include "axi_txn_request_item.svh"
+  `include "axi_read_data_item.svh"
+endpackage
